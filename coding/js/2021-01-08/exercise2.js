ac245316a49bf7d89522c627c7f3bbe3ef3a16a3
Rohit Lalwani <lalwanirohit111@gmail.com>
2021-01-10 16:46:09 +0530

    made some changes

--- coding/js/2021-01-08/exercise2.js
+++ coding/js/2021-01-08/exercise2.js
@@ -1,35 +1,37 @@
 //john's team and mark's team average
 var johnTeamAverage , markTeamAverage;
 
-johnTeamAverage = (188 + 120 + 103) / 3;
+johnTeamAverage = (89 + 120 + 103) / 3;
 markTeamAverage = (116 + 94 + 123) / 3;
-
-//compare two values using ternery operator 
-var winner = (johnTeamAverage > markTeamAverage) ? 
-console.log("the winner is john's basketball team with an average of " + johnTeamAverage) :
-console.log("the winner is mark's basketball team with an average of " + markTeamAverage) ;
 
 //comparing two values using simple if statement
 if(johnTeamAverage > markTeamAverage){
     console.log("the winner is john's basketball team with an average of " + johnTeamAverage );
 }
-else{
+else if (markTeamAverage > johnTeamAverage){
     console.log("the winner is mark's basketball team with an average of " + markTeamAverage );
+}
+else{
+    console.log("draw between mark and john");
 }
 
 // mary's team average 
 var maryTeamAverage;
-maryTeamAverage = (115 + 99 + 200) / 3;
+maryTeamAverage = (97 + 134 + 105) / 3;
 
 // compare more than two values usinf if....else if....else statement 
 if(johnTeamAverage > markTeamAverage && johnTeamAverage > maryTeamAverage){
     console.log("the winner is john's basketball team with an average of " + johnTeamAverage);
 }
-else if(maryTeamAverage > maryTeamAverage && markTeamAverage > johnTeamAverage){
+else if(markTeamAverage > maryTeamAverage && markTeamAverage > johnTeamAverage){
     console.log("the winner is mark's basketball team with an average of " + markTeamAverage);
 }
-else{
+else if(maryTeamAverage > johnTeamAverage && maryTeamAverage > markTeamAverage){
     console.log("the winner is mary's basketball team with an average of " + maryTeamAverage);
 }
+else{
+    console.log("draw between mark , john and mary")
+}
+
     
 
