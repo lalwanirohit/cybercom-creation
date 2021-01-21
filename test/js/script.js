bd3f18d2b971bd88735c0e11cb7a82165b1ae240
Rohit Lalwani <lalwanirohit111@gmail.com>
2021-01-21 12:34:26 +0530

    second commit

--- test/js/script.js
+++ test/js/script.js
@@ -1,23 +1,23 @@
 
-var array = [];
-function addAdmin()
-{
-	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-    var pass = document.getElementById('password').value;
-    var city = document.getElementById('city').value;
-    var state = document.getElementById('state').value;
-	 var admin = {
-		name : name,
-		email : email,
-        password : pass,
-        city : city,
-        state : state
-    };
+// var array = [];
+// function addAdmin()
+// {
+// 	var name = document.getElementById('name').value;
+// 	var email = document.getElementById('email').value;
+//     var pass = document.getElementById('password').value;
+//     var city = document.getElementById('city').value;
+//     var state = document.getElementById('state').value;
+// 	 var admin = {
+// 		name : name,
+// 		email : email,
+//         password : pass,
+//         city : city,
+//         state : state
+//     };
 
-	array.push(admin);
-	localStorage.setItem("admin", admin);
-	console.log(`admin registered successfully`);
+// 	array.push(admin);
+// 	localStorage.setItem("admin", admin);
+// 	console.log(`admin registered successfully`);
 	
-};
-document.getElementById("reg").addEventListener("click", addAdmin());
+// };
+// document.getElementById("reg").addEventListener("click", addAdmin());
